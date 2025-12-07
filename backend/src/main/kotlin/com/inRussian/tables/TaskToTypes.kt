fe8f922a470c821f10747e3beb1a34e69efd5d5f
Dark-Type <SKAZHUTIN.N@MAIL.RU>
2025-12-07 22:16:55 +0700

    fix of cascade deletion

--- backend/src/main/kotlin/com/inRussian/tables/TaskToTypes.kt
+++ backend/src/main/kotlin/com/inRussian/tables/TaskToTypes.kt
@@ -1,7 +1,12 @@
 package com.inRussian.tables
+import org.jetbrains.exposed.sql.ReferenceOption
 import org.jetbrains.exposed.sql.Table
 
 object TaskToTypes : Table("task_to_types") {
-    val task = reference("task_id", TaskEntity)
+    val task = reference(
+        name = "task_id",
+        foreign = TaskEntity,
+        onDelete = ReferenceOption.CASCADE
+    )
     val typeName = reference("type_name", TaskTypes.name)
 }
